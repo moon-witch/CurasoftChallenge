5bb0646e70d5d67a881ae7b8485304a9ee8906d6
moonwitch <joshua_develops@proton.me>
2025-03-09 17:13:20 +0100

    feat: added FilterButton for filtering of the list

---
+++ src/components/LogTable/FilterButton.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const emit = defineEmits(["filter"]);
+
+type Filter = "Alle" | "Fehler" | "Warnung" | "Log";
+const currentFilter = ref<Filter>("Alle");
+
+const changeFilter = (newFilter: Filter) => {
+  currentFilter.value = newFilter;
+  emit("filter", currentFilter.value);
+};
+</script>
+
+<template>
+  <section class="filter">
+    <button
+      class="filter-button"
+      :class="{ active: currentFilter === 'Alle' }"
+      @click="changeFilter('Alle')"
+    >
+      Alle
+    </button>
+    <button
+      class="filter-button"
+      :class="{ active: currentFilter === 'Fehler' }"
+      @click="changeFilter('Fehler')"
+    >
+      Fehler
+    </button>
+    <button
+      class="filter-button"
+      :class="{ active: currentFilter === 'Warnung' }"
+      @click="changeFilter('Warnung')"
+    >
+      Warnung
+    </button>
+    <button
+      class="filter-button"
+      :class="{ active: currentFilter === 'Log' }"
+      @click="changeFilter('Log')"
+    >
+      Log
+    </button>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.filter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 1.5rem;
+
+  .filter-button {
+    background: var(--accent-color);
+    border: none;
+    padding: 0.5rem 1rem;
+    color: white;
+    opacity: 0.5;
+    cursor: pointer;
+    &:first-of-type {
+      border-top-left-radius: 4px;
+      border-bottom-left-radius: 4px;
+    }
+    &:last-of-type {
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
+    }
+    &.active {
+      opacity: 1;
+    }
+  }
+}
+</style>
--- src/components/LogTable/LogTable.vue
+++ src/components/LogTable/LogTable.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import FilterButton from "@/components/LogTable/FilterButton.vue";
+
 defineProps({
   searchString: {
     type: String,
   },
 });
+
+const currentFilter = ref('Alle')
+const changeFilter = (event: Record<string, any>) => {
+  currentFilter.value = event.target.textContent;
+}
 </script>
 
 <template>
-  <div>{{ searchString }}</div>
+  <FilterButton @click="changeFilter($event)" />
+  {{ currentFilter }}
+  <section class="list"></section>
+  <section class="pagination"></section>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+
+</style>
